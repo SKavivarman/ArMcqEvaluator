3c58c8aa7d386c627446927fed2db8c961bb952c
SharuVive <vivesharu@gmail.com>
2016-11-18 07:05:49 +0530

    Navigating to get Subject details from user.

--- app/src/main/java/com/example/kathir/armcqevaluator/MainActivity.java
+++ app/src/main/java/com/example/kathir/armcqevaluator/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.kathir.armcqevaluator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button evaluateAns;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        evaluateAns = (Button) findViewById(R.id.btnEvaluateAns);
+
+        evaluateAns.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent st = new Intent(MainActivity.this, SubjectDetailActivity.class); //redirecting to another activity
+                startActivity(st);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/kathir/armcqevaluator/SubjectDetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.kathir.armcqevaluator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sharu Vive on 11/18/2016.
+ */
+
+public class SubjectDetailActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_detail);
+
+    }
+}
