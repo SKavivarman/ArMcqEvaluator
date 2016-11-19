d5a17d8a217a796b2ddc35928624ccd9a6123b4f
SharuVive <vivesharu@gmail.com>
2016-11-20 01:03:00 +0530

    SubjectDetailActivity design changed. onCreate method partially fininshed.

--- app/src/main/java/com/example/kathir/armcqevaluator/SubjectDetailActivity.java
+++ app/src/main/java/com/example/kathir/armcqevaluator/SubjectDetailActivity.java
@@ -2,6 +2,8 @@ package com.example.kathir.armcqevaluator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 /**
  * Created by Sharu Vive on 11/18/2016.
@@ -9,9 +11,21 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SubjectDetailActivity extends AppCompatActivity {
 
+    RadioGroup radioSemesterGroup;
+    RadioGroup radioYearGroup;
+
+    Button subDetailsOK;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_detail);
+
+        radioSemesterGroup =(RadioGroup)findViewById(R.id.radGroupSeme);
+        radioYearGroup = (RadioGroup)findViewById(R.id.radGroupYear);
+
+        subDetailsOK = (Button)findViewById(R.id.btnsubDetailsOK);
+
+
 
     }
 }
